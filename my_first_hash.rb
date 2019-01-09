de23eca4c1b04c874d48a2e8fe714de990070e3d
David Charles <ratlover03@gmail.com>
2019-01-09 17:17:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  heres_the_hash = {"john" => "idk", "doe" => "i still dk"}
 end
 
 
